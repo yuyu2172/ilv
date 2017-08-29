f75f3643c6aaf072ea779c80996fa2afee2294f7
Yusuke Niitani <yuyuniitani@gmail.com>
2017-08-29 10:25:56 +0900

    include experiment script in utils

--- ilv/utils.py
+++ ilv/utils.py
@@ -1,4 +1,9 @@
+from itertools import izip
+from itertools import product
+import os
 import pandas as pd
+import subprocess
+
 
 def list_of_dict_to_dict_of_list(list_of_dict):
     # http://stackoverflow.com/questions/5558418/list-of-dicts-to-from-dict-of-lists
@@ -22,3 +27,20 @@ def filter_dict(d, keys):
 def moving_average_1d(list_, window):
     series = pd.Series(list_)
     return series.rolling(window=window, center=True).mean().tolist()
+
+
+def dict_of_list_to_list_of_dict(dicts):
+    return list(dict(izip(dicts, x)) for x in product(*dicts.itervalues()))
+
+
+def run_experiments(options, base_cmd, date_dir):
+    options_list = dict_of_list_to_list_of_dict(options)
+    for i, opts in enumerate(options_list):
+        cmd = base_cmd
+        out = os.path.join(date_dir, 'iter_{}'.format(i))
+        cmd += ' --out {} '.format(out)
+
+        for key, val in opts.items():
+            cmd += ' --{} {} '.format(key, val)
+        print(cmd)
+        subprocess.call(cmd, shell=True)
